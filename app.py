dbb9eb7f57ac2eaf1ed892065514613101bae4a2
Karl Alea <128546810+Krill-alea@users.noreply.github.com>
2025-03-05 11:16:10 +1300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# very small simple testing module
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#routes go here
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#this bit of code runs the app that we just made with debug on
+if __name__ == "__main__":
+    app.run(debug=True)
